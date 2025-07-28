3b9252ef03c6e147766f118335e04ae21d823a8a
Chester Sim <chestersimdy@gmail.com>
2025-07-28 11:58:16 +0800

    refactor: bump common

--- ui/next.config.js
+++ ui/next.config.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const nextConfig = {
   webpack(config, { isServer }) {
     if (!isServer) {
@@ -16,27 +18,24 @@ const nextConfig = {
 
     return config;
   },
-  experimental: {
-    turbo: {
-      useSwcCss: true,
-      root: "..",
-      resolveAlias: {
-        "@drift-labs/sdk": "../drift-common/protocol/sdk",
-        "@drift-labs/icons": "../drift-common/icons/dist/",
-        "@drift/common": "../drift-common/common-ts/lib/index.js",
-        "@drift-labs/react": "../drift-common/react/lib/index.js",
-        react: "./node_modules/@types/react",
-        fs: { browser: "./node-browser-compatibility.js" },
-        net: { browser: "./node-browser-compatibility.js" },
-        dns: { browser: "./node-browser-compatibility.js" },
-        tls: { browser: "./node-browser-compatibility.js" },
-        crypto: { browser: "crypto-browserify" },
-      },
-      rules: {
-        "*.svg": {
-          loaders: ["@svgr/webpack"],
-          as: "*.js",
-        },
+  turbopack: {
+    resolveAlias: {
+      "@drift-labs/sdk": "../drift-common/protocol/sdk",
+      "@drift-labs/icons": "../drift-common/icons/dist/",
+      "@drift/common": "../drift-common/common-ts/lib/index.js",
+      "@drift-labs/react": "../drift-common/react/lib/index.js",
+      react: "./node_modules/@types/react",
+      fs: { browser: "./node-browser-compatibility.js" },
+      net: { browser: "./node-browser-compatibility.js" },
+      dns: { browser: "./node-browser-compatibility.js" },
+      tls: { browser: "./node-browser-compatibility.js" },
+      crypto: { browser: "crypto-browserify" },
+    },
+    root: path.resolve(__dirname, ".."),
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
       },
     },
   },
